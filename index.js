3367dabe0732fd0789dc94a2e5ec6897c96317d7
Sean Altemose <sean@seanaltemose.com>
2019-04-27 17:32:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+  var counter = 0;
+
+  function addBump(){
+    counter = counter +1;
+  }
+
+  function getBumps(){
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+
+}
+
+function createAnimal(animalType){
+  function diabolicalCreation(deadlyDevice){
+    var obj = {
+      "animalType" : animalType,
+      "deadlyDevice" : deadlyDevice
+    }
+    return obj
+  }
+  return diabolicalCreation
+}
+
+
+  var sharkCreator = createAnimal('Shark')
+  var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+  var sharkWithFrickinCannon = sharkCreator('Cannon')
+
+  console.log(sharkWithFrickinCannon)
